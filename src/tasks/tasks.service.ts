8a869e30e817865bc5ba5cb2d4f58fa7fbb0e8cc
Ariel Rozen <rozen.ariel@gmail.com>
2022-05-15 12:54:54 +0300

    wip

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -3,7 +3,6 @@ import { Task, TaskStatus } from './task.model';
 import { v4 as uuid } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
-import { filter } from 'rxjs';
 
 @Injectable()
 export class TasksService {
